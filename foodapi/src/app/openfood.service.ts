c3bf7ce806c3522339094bb6dfe60b1808075ae9
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2024-09-04 16:26:48 +0000

    punto 4

---
+++ foodapi/src/app/openfood.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class OpenfoodService {
+
+  constructor(private http: HttpClient) { }
+  searchFood(query: string) {
+    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=5&json=true`; 
+    let obsFood = this.http.get(url);
+    console.log(obsFood)
+    return obsFood;}
+}
--- foodapi/src/app/search/search.component.ts
+++ foodapi/src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OpenfoodService } from '../openfood.service';
+
 
 @Component({
   selector: 'app-search',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  query !: string;
+  obsFood !: Observable<Object>;
+  results: any;
 
+  
+constructor(public cercacibo : OpenfoodService) {}
+
+  submit(query: HTMLInputElement): void {
+
+    if (!query.value) {
+      return;
+    }
+
+    this.query = query.value;
+    this.obsFood = this.cercacibo.searchFood(this.query);
+    this.obsFood.subscribe((data) => { this.results = data;});
+  }
 }
